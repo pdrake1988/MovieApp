2963b2c0db0dfae01510a6cebccc3a1d0f9d3b5a
pdrake1988 <pdrake1@me.com>
2022-07-30 11:46:57 -0500

    done most of the way

---
+++ ApplicationCore/Entites/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entites
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public Movie MovieId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ ApplicationCore/Entites/Trailer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entites
+{
+    public class Trailer
+    {
+        public int Id { get; set; }
+        public Movie MovieId { get; set; }
+
+        [Column(TypeName = "Varchar(2084)")]
+        public string TrailerUrl { get; set; }
+
+        [Column(TypeName = "Varchar(2084)")]
+        public string Name { get; set; }
+    }
+}
